aa967bd57e57072ef2aafb601c89aede92ee3c7b
Joda TheCoda <jodathecoda@yahoo.com>
2022-02-04 15:22:42 +0200

    ready

--- bw.py
+++ bw.py
@@ -129,48 +129,16 @@ if len(sys.argv) > 1:
 #incognito = 1
 
 if incognito:
-    print("1-34")
+    print("1-9")
 else:
-    print("1 bachmann.juergen vs deepstack.ai")
-    print("2 bos.alexander vs deepstack.ai")
-    print("3 dmit.pol vs deepstack.ai")
-    print("4 eshkar.eyal vs deepstack.ai")
-    print("5 freire.gaia vs deepstack.ai")
-    print("6 gavin.fintan vs deepstack.ai")
-    print("7 indenok.sergey vs deepstack.ai")
-    print("8 islam.jefri vs deepstack.ai")
-    print("9 laak.phil vs deepstack.ai")
-    print("10 lesnoy.dmitry vs deepstack.ai")
-    print("11 moschitta.luca vs deepstack.ai")
-    print("12 naumenko.igor vs deepstack.ai")
-    print("13 okearney.dara vs deepstack.ai")
-    print("14 parlavecchio.antonio vs deepstack.ai")
-    print("15 pastor.juan_manuel vs deepstack.ai")
-    print("16 phan.mike vs deepstack.ai")
-    print("17 pizzarello.silvio vs deepstack.ai")
-    print("18 qin.youwei vs deepstack.ai")
-    print("19 santos.victor vs deepstack.ai")
-    print("20 schaumann.lucas vs deepstack.ai")
-    print("21 schwab.sebastian vs deepstack.ai")
-    print("22 sethi.muskan vs deepstack.ai")
-    print("23 shabalin.ivan vs deepstack.ai")
-    print("24 shaposhnikov.roman vs deepstack.ai")
-    print("25 shrimankar.prakshat vs deepstack.ai")
-    print("26 sturc.martin vs deepstack.ai")
-    print("27 sun.fan vs deepstack.ai")
-    print("28 sun.kaishi vs deepstack.ai")
-    print("29 takeda.tsuneaki vs deepstack.ai")
-    print("30 talacka.giedrius vs deepstack.ai")
-    print("31 tishekvich.stas vs deepstack.ai")
-    print("32 voloshin.stanislav vs deepstack.ai")
-    print("33 zurr.shai vs deepstack.ai")
-    print("34 fedor.holz vs wiktor.malinowski")
+    print("select hand history file (1-9)")
+
 try:
     pl = int(input("choose number: "))
 except:
-    print("enter number 1-34")
+    print("enter number 1-9")
 
-if pl > 0 and pl < 35:
+if pl > 0 and pl < 10:
     pass
 else:
     print("error file number")
@@ -178,110 +146,25 @@ else:
 hero = ""
 villain = "Villain"
 villain_nickname = "Villain"
+hero = "Hero"
 if pl == 1:
-    hero = "Hero"
     f = open(cwd + '\\hh\\HH1.txt',"r")
 elif pl == 2:
-    hero = "bos.alexander"
-    f = open(cwd + '\\full_info\\all_hands.bos.alexander.log',"r")
+    f = open(cwd + '\\hh\\HH2.txt',"r")
 elif pl == 3:
-    hero = "dmit.pol" 
-    f = open(cwd + '\\full_info\\all_hands.dmit.pol.log',"r")
+    f = open(cwd + '\\hh\\HH3.txt',"r")
 elif pl == 4:
-    hero = "eshkar.eyal"
-    f = open(cwd + '\\full_info\\all_hands.eshkar.eyal.log',"r")
+    f = open(cwd + '\\hh\\HH4.txt',"r")
 elif pl == 5:
-    hero = "freire.gaia" 
-    f = open(cwd + '\\full_info\\all_hands.freire.gaia.log',"r")
+    f = open(cwd + '\\hh\\HH5.txt',"r")
 elif pl == 6:
-    hero = "gavin.fintan" 
-    f = open(cwd + '\\full_info\\all_hands.gavin.fintan.log',"r")
+    f = open(cwd + '\\hh\\HH6.txt',"r")
 elif pl == 7:
-    hero = "indenok.sergey"
-    f = open(cwd + '\\full_info\\all_hands.indenok.sergey.log',"r")
+    f = open(cwd + '\\hh\\HH7.txt',"r")
 elif pl == 8:
-    hero = "islam.jefri"
-    f = open(cwd + '\\full_info\\all_hands.islam.jefri.log',"r")
+    f = open(cwd + '\\hh\\HH8.txt',"r")
 elif pl == 9:
-    hero = "laak.phil"
-    f = open(cwd + '\\full_info\\all_hands.laak.phil.log',"r")
-elif pl == 10:
-    hero = "lesnoy.dmitry"
-    f = open(cwd + '\\full_info\\all_hands.lesnoy.dmitry.log',"r")
-elif pl == 11:
-    hero = "moschitta.luca"
-    f = open(cwd + '\\full_info\\all_hands.moschitta.luca.log',"r")
-elif pl == 12:
-    hero = "naumenko.igor"
-    f = open(cwd + '\\full_info\\all_hands.naumenko.igor.log',"r")
-elif pl == 13:
-    hero = "okearney.dara"
-    f = open(cwd + '\\full_info\\all_hands.okearney.dara.log',"r")
-elif pl == 14:
-    hero = "parlavecchio.antonio"
-    f = open(cwd + '\\full_info\\all_hands.parlavecchio.antonio.log',"r")
-elif pl == 15:
-    hero = "pastor.juan_manuel"
-    f = open(cwd + '\\full_info\\all_hands.pastor.juan_manuel.log',"r")
-elif pl == 16:
-    hero = "phan.mike"
-    f = open(cwd + '\\full_info\\all_hands.phan.mike.log',"r")
-elif pl == 17:
-    hero = "pizzarello.silvio"
-    f = open(cwd + '\\full_info\\all_hands.pizzarello.silvio.log',"r")
-elif pl == 18:
-    hero = "qin.youwei"
-    f = open(cwd + '\\full_info\\all_hands.qin.youwei.log',"r")
-elif pl == 19:
-    hero = "santos.victor"
-    f = open(cwd + '\\full_info\\all_hands.santos.victor.log',"r")
-elif pl == 20:
-    hero = "schaumann.lucas"
-    f = open(cwd + '\\full_info\\all_hands.schaumann.lucas.log',"r")
-elif pl == 21:
-    hero = "schwab.sebastian"
-    f = open(cwd + '\\full_info\\all_hands.schwab.sebastian.log',"r")
-elif pl == 22:
-    hero = "sethi.muskan"
-    f = open(cwd + '\\full_info\\all_hands.sethi.muskan.log',"r")
-elif pl == 23:
-    hero = "shabalin.ivan"
-    f = open(cwd + '\\full_info\\all_hands.shabalin.ivan.log',"r")
-elif pl == 24:
-    hero = "shaposhnikov.roman"
-    f = open(cwd + '\\full_info\\all_hands.shaposhnikov.roman.log',"r")
-elif pl == 25:
-    hero = "shrimankar.prakshat"
-    f = open(cwd + '\\full_info\\all_hands.shrimankar.prakshat.log',"r")
-elif pl == 26:
-    hero = "sturc.martin"
-    f = open(cwd + '\\full_info\\all_hands.sturc.martin.log',"r")
-elif pl == 27:
-    hero = "sun.fan"
-    f = open(cwd + '\\full_info\\all_hands.sun.fan.log',"r")
-elif pl == 28:
-    hero = "sun.kaishi"
-    f = open(cwd + '\\full_info\\all_hands.sun.kaishi.log',"r")
-elif pl == 29:
-    hero = "takeda.tsuneaki"
-    f = open(cwd + '\\full_info\\all_hands.takeda.tsuneaki.log',"r")
-elif pl == 30:
-    hero = "talacka.giedrius"
-    f = open(cwd + '\\full_info\\all_hands.talacka.giedrius.log',"r")
-elif pl == 31:
-    hero = "tishekvich.stas"
-    f = open(cwd + '\\full_info\\all_hands.tishekvich.stas.log',"r")
-elif pl == 32:
-    hero = "voloshin.stanislav"
-    f = open(cwd + '\\full_info\\all_hands.voloshin.stanislav.log',"r")
-elif pl == 33:
-    hero = "zurr.shai"
-    f = open(cwd + '\\full_info\\all_hands.zurr.shai.log',"r")
-elif pl == 34:
-    hero = "fedor.holz"
-    f = open(cwd + '\\full_info\\all_hands.fedor.holz.log',"r")
-    villain = "wiktor.malinowski"
-    villain_nickname = "wiktor.malinowski"
+    f = open(cwd + '\\hh\\HH9.txt',"r")
 else:
     pass
 
